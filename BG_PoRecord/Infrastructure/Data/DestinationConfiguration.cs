a6c9917a60a7a573e570cadbe31f337215772db9
PaeFaceDev <std.64122420210@ubru.ac.th>
2026-06-27 13:10:36 +0700

    Connect DbContext & SAP Connect

---
+++ BG_PoRecord/Infrastructure/Data/DestinationConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAP.Middleware.Connector;
+
+namespace BG_PoRecord.Infrastructure.Data
+{
+    internal class DestinationConfiguration : IDestinationConfiguration
+    {
+        public static string appName = ConfigurationManager.AppSettings["appName"];
+
+        public bool ChangeEventsSupported()
+        {
+            return true;
+        }
+
+        public event RfcDestinationManager.ConfigurationChangeHandler ConfigurationChanged;
+
+        public RfcConfigParameters GetParameters(string destinationName)
+        {
+            if (destinationName.Equals(appName))
+            {
+                RfcConfigParameters parms = new RfcConfigParameters();
+                parms.Add(RfcConfigParameters.AppServerHost, ConfigurationManager.AppSettings["AppServerHost"]);
+                parms.Add(RfcConfigParameters.SystemNumber, ConfigurationManager.AppSettings["SystemNumber"]);
+                parms.Add(RfcConfigParameters.SystemID, ConfigurationManager.AppSettings["SystemID"]);
+                parms.Add(RfcConfigParameters.User, ConfigurationManager.AppSettings["User"]);
+                parms.Add(RfcConfigParameters.Password, ConfigurationManager.AppSettings["Password"]);
+                parms.Add(RfcConfigParameters.Client, ConfigurationManager.AppSettings["Client"]);
+                parms.Add(RfcConfigParameters.Language, ConfigurationManager.AppSettings["Language"]);
+                return parms;
+            }
+            else
+            {
+                throw new Exception("Destination configuration not found for " + destinationName);
+            }
+        }
+    }
+}
---
+++ BG_PoRecord/Infrastructure/Data/PostgreSqlDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BG_PoRecord.Infrastructure.Data
+{
+    internal class PostgreSqlDbContext : DbContext
+    {
+        public PostgreSqlDbContext(DbContextOptions<PostgreSqlDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = ConfigurationManager.AppSettings["npgConnString"];
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(
+                    connectionString,
+                    connection => connection.CommandTimeout(300)
+                    );
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity mappings here
+        }
+    }
+}
